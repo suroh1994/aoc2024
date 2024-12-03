5d94fbbc0550ceb5d7c9408d88dd7560055e5471
Sebastian <suroh1994@gmx.de>
2024-12-03 11:10:43 +0100

    Simplify Day 3

--- 3/main.go
+++ 3/main.go
@@ -7,46 +7,32 @@ import (
 )
 
 func main() {
-	content := lib.ReadInputAsLines()
-	totalPart1 := 0
-	totalPart2 := 0
-	toggle := true
-	for _, line := range content {
-		totalPart1 += calculateInstructions(line)
-		totalPart2 += calculateInstructionsWithToggles(line, &toggle)
-	}
+	content := lib.ReadInput()
+	totalPart1, totalPart2 := calculateInstructions(content)
 
 	fmt.Printf("Part 1: %d\n", totalPart1)
 	fmt.Printf("Part 2: %d\n", totalPart2)
 }
 
-var regex = regexp.MustCompile("mul\\((\\d{1,3}),(\\d{1,3})\\)")
-
-func calculateInstructions(line string) int {
-	matches := regex.FindAllStringSubmatch(line, -1)
-	lineTotal := 0
-	for _, match := range matches {
-		lineTotal += lib.MustParseToInt(match[1]) * lib.MustParseToInt(match[2])
-	}
-	return lineTotal
-}
-
 var regexWithToggles = regexp.MustCompile("mul\\((\\d{1,3}),(\\d{1,3})\\)|do(n't)*\\(\\)")
 
-func calculateInstructionsWithToggles(line string, toggle *bool) int {
+func calculateInstructions(line string) (int, int) {
 	matches := regexWithToggles.FindAllStringSubmatch(line, -1)
-	lineTotal := 0
+	total := 0
+	toggle := true
+	totalWithToggle := 0
 	for _, match := range matches {
 		switch match[0] {
 		case "do()":
-			*toggle = true
+			toggle = true
 		case "don't()":
-			*toggle = false
+			toggle = false
 		default:
-			if *toggle {
-				lineTotal += lib.MustParseToInt(match[1]) * lib.MustParseToInt(match[2])
+			total += lib.MustParseToInt(match[1]) * lib.MustParseToInt(match[2])
+			if toggle {
+				totalWithToggle += lib.MustParseToInt(match[1]) * lib.MustParseToInt(match[2])
 			}
 		}
 	}
-	return lineTotal
+	return total, totalWithToggle
 }
